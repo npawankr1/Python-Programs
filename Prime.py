a25b662dd12cd13235ce072a969b3a3d3c6af498
MrPerfect2530 <72927675+MrPerfect2530@users.noreply.github.com>
2020-10-15 19:34:36 +0530

    Create Prime.py
    A program to check given number is prime or not.

---
+++ Prime.py
@@ -0,0 +1,23 @@
+# https://www.facebook.com/harhar.modi.1232/posts/706155836649246
+# Subscribed by Jayant Gupta
+
+# Program to check if a number is prime or not
+
+num = 407
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+else:
+   print(num,"is not a prime number")
+
