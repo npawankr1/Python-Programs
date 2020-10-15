b3de3abc36171a620640dd3e2049714583158933
Naveen_Yadav <66903732+NovaKing69@users.noreply.github.com>
2020-10-15 21:26:06 +0530

    added largest number

---
+++ largest number .py
@@ -0,0 +1,14 @@
+#https://www.facebook.com/permalink.php?story_fbid=104283864796030&id=100056432505093
+# subscribed by Naveen Yadav
+
+# Python program to find largest 
+# number in a list 
+
+# list of numbers 
+list1 = [10, 20, 4, 45, 99] 
+
+# sorting the list 
+list1.sort() 
+
+# printing the last element 
+print("Largest element is:", list1[-1]) 
